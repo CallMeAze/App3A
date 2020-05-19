3c6413d6c9fc80bff1077b166b68e839627c3582
CallMeAze <alexis.malderez@gmail.com>
2020-05-19 14:29:49 +0200

    ajout du serv

--- app/src/main/java/com/example/app3a/model/api/API.java
+++ app/src/main/java/com/example/app3a/model/api/API.java
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface API {
-    @GET("index.php")
+    @GET("projet3a.php")
     Call<List<Personnage>> get();
 }
--- app/src/main/java/com/example/app3a/model/api/Link.java
+++ app/src/main/java/com/example/app3a/model/api/Link.java
@@ -1,5 +1,6 @@
 package com.example.app3a.model.api;
 
 public class Link {
-
+    public static String api         = "http://40.89.173.11/proj3/";
+    public static String image       = "http://40.89.173.11/proj3/";
 }
